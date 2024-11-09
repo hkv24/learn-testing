f2127abde4167401c411e4e79e20947fe1cba7bc
hkv24 <harshkumarverma99@gmail.com>
2024-11-09 23:52:11 +0530

    commit

--- 3-express-zod/dist/index.js
+++ 3-express-zod/dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("Hellow");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const sumInput = zod_1.z.object({
+    a: zod_1.z.number(),
+    b: zod_1.z.number()
+});
+app.post('/sum', (req, res) => {
+    const parsedResponse = sumInput.safeParse(req.body);
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Invalid input",
+        });
+        return;
+    }
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+    res.json(answer);
+});
---
+++ 3-express-zod/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import { z } from "zod";
+
+const app = express()
+app.use(express.json());
+
+const sumInput =z.object({
+    a: z.number(),
+    b: z.number()
+})
+
+app.post('/sum', (req: Request, res: Response) => {
+    const parsedResponse = sumInput.safeParse(req.body);
+
+    if(!parsedResponse.success) {
+        res.status(411).json({
+            message: "Invalid input",
+        })
+        return ;
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b
+
+    res.json(answer)
+})
