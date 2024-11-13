3b8e731d2cb402e5d6617667142f937db8c55840
hkv24 <harshkumarverma99@gmail.com>
2024-11-13 23:54:45 +0530

    removed dist from assognment-6

--- 6-mocking-return-values/dist/__mocks__/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaClient = void 0;
-const vitest_mock_extended_1 = require("vitest-mock-extended");
-exports.prismaClient = (0, vitest_mock_extended_1.mockDeep)();
